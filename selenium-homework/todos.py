ced1a1c4e9daa3c58a9ea78e1081990317741c1d
Kincses László <kincsesster@gmail.com>
2021-06-23 15:28:31 +0200

    Az todos.py házi feladat feltöltése.

---
+++ selenium-homework/todos.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("http://localhost:9999/todo.html")
+
+todo_k = driver.find_elements_by_class_name("done-false")
+
+for x in todo_k:
+    print(x.text)
+
+#driver.close()
