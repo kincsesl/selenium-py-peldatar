0aba0ff64b72db7f62ddc45976a3672b4ffbc53d
Kincses László <kincsesster@gmail.com>
2021-06-28 16:51:04 +0200

    Selenium2-házik csvszűrés-0.0 verzió.

---
+++ selenium2-homework/csvszures.py
@@ -0,0 +1,8 @@
+import csv
+
+with open("table_in.csv", newline="\n") as file:
+    reader = csv.reader(file)
+    for i in reader:
+        print(i)
+
+
--- selenium2-homework/linkek.py
+++ selenium2-homework/linkek.py
@@ -1,4 +1,3 @@
-import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,8 +15,8 @@ try:
 
     with open("linkek.txt", "w") as f:
         for sor in sorok:
-            f.write(sor.get_attribute("href")+"\n")
-        
+            f.write(sor.get_attribute("href") + "\n")
+    print(len(sorok))
     driver.close()
 except:
     print("Nem nyílt meg az oldal.")
