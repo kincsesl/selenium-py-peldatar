772d443e38b5b9372a79f6d67c2cc696c7b31d97
Kincses László <kincsesster@gmail.com>
2021-07-07 15:43:45 +0200

    Edittable kész.

--- selenium-homework/proba_sajat.py
+++ selenium-homework/proba_sajat.py
@@ -16,19 +16,19 @@ def beír(mező, s):
     print("Érték:", mező.get_attribute("value"))
 
 
-#date = driver.find_element_by_id("example-input-date")
-#datetime = driver.find_element_by_id("example-input-date-time")
-#datetime_local = driver.find_element_by_id("example-input-date-time-local")
+date = driver.find_element_by_id("date")
+datetime = driver.find_element_by_id("time")
+datetime_local = driver.find_element_by_id("datetime-local")
 month = driver.find_element_by_id("month")
 week = driver.find_element_by_id("week")
-#time = driver.find_element_by_id("example-input-time")
+time = driver.find_element_by_id("time")
 
-#beír(date, "002021-07-09")
-#beír(datetime, "1999.08.13 12:00")
-#beír(datetime_local, "002021-07-08T21:20")
+beír(date, "2021-07-09")
+beír(datetime, "1999.08.13 12:00")
+beír(datetime_local, "002021-07-08T21:20")
 beír(month, "2021\tjúnius")
 beír(week, "12\t2021")
-#beír(time, "21:20")
+beír(time, "21:20")
 
 
 
