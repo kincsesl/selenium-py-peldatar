84297954fe60a2cf36cad08031c8a08e3f21e01b
Kincses László <kincsesster@gmail.com>
2021-07-09 19:56:43 +0200

    Megvan a videós is (kis szépséghibával).

---
+++ selenium2-homework/videoooo.py
@@ -0,0 +1,34 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+# options.headless = True
+driver = webdriver.Chrome()
+
+driver.get("http://localhost:9999/videos.html")
+
+
+def indítmegállít(v):
+    v.click()
+    v.send_keys(Keys.SPACE)
+    time.sleep(2)
+    v.send_keys(Keys.SPACE)
+
+
+videó = driver.find_element_by_tag_name("video")
+
+indítmegállít(videó)
+
+ketteske = driver.find_element_by_xpath("/html/body/div/button[1]")
+ketteske.click()
+time.sleep(2)
+ketteske.click()
+
+hármaska = driver.find_element_by_xpath("/html/body/iframe")
+hármaska.click()
+hármaska.send_keys(Keys.SPACE)
+time.sleep(2)
+hármaska.send_keys(Keys.SPACE)
+driver.close()
