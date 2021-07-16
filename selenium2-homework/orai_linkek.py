4b139038d7667456bd36138011a7f59bbf9b24cb
Kincses László <kincsesster@gmail.com>
2021-07-16 10:56:06 +0200

    Már megy.

--- selenium2-homework/orai_linkek.py
+++ selenium2-homework/orai_linkek.py
@@ -13,17 +13,21 @@ from selenium.webdriver.common.keys import Keys
 driver.get("https://witty-hill-0acfceb03.azurestaticapps.net/hackernews/index.html#")
 
 linkek = []
-"""
-div_valami = driver.find_element_by_id("router-outlet")
-hrefek = div_valami.find_elements_by_class_name("story")
+
+time.sleep(3)
+
+hrefek = driver.find_elements_by_class_name("story")
 
 for href in hrefek:
-    linkek.append(href.get_attribute("href"))
-"""
+    a = href.find_element_by_tag_name("a")
+    linkek.append(a.get_attribute("href"))
+
+print(linkek)
+
+linkek = []
 
 for i in range(1, 31):
     xp = '/html/body/div/div[2]/div/div/div['+str(i)+']/div[1]/a'
-    print(xp)
     linkek.append(driver.find_element_by_xpath(xp).get_attribute("href"))
 
 print(linkek)
