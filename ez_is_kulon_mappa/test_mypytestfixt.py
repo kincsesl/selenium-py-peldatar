7581e762ea6a782a4bc72364a233c12160b46b11
Kincses László <kincsesster@gmail.com>
2021-07-22 15:53:21 +0200

    mypytestfixt.py

---
+++ ez_is_kulon_mappa/test_mypytestfixt.py
@@ -0,0 +1,34 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope='session')  # scope="module","determine_scope","class","package"
+def browser():
+    options = Options()
+    # options.headless = True
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://localhost:9999/filltablewithsum.html")
+    return driver
+
+
+def test_first_testcase(browser):
+    összegtábla = browser.find_element_by_id("resultTotals")
+    quantitytotals = összegtábla.find_element_by_id("qtyTotals")
+    pricetotals = összegtábla.find_element_by_id("priceTotals")
+    tartalmazó_form = browser.find_element_by_id("order")
+    productmező = tartalmazó_form.find_element_by_id("product")
+    quantitymező = tartalmazó_form.find_element_by_id("quantity")
+    pricemező = tartalmazó_form.find_element_by_id("price")
+    resetgomb = tartalmazó_form.find_element_by_id("resetbtn")
+    addgomb = tartalmazó_form.find_element_by_id("add")
+    assert addgomb.text == "Add To Table"
+
+
+def test_addgomb(browser):
+    driver = browser
+    driver.get("http://localhost:9999/another_form.html")
+    time.sleep(2)
+    assert True
+    # assert oldalteszt.addgomb.text == "Add To Table"
